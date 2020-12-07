d4b2a8fd6d015f70f5f46d2c9f1cecc5ba44d3ef
Jorge Costa <jorge.costa@thetrainline.com>
2020-12-07 20:07:01 +0000

    day 4 ex 2

--- day4.js
+++ day4.js
@@ -292,28 +292,67 @@ const example = [
   "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929",
   "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm",
   "hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in\n"
-]
+];
 
-const requiredFields = [
-  "byr", //(Birth Year)
-  "iyr", //(Issue Year)
-  "eyr", //(Expiration Year)
-  "hgt", //(Height)
-  "hcl", //(Hair Color)
-  "ecl", //(Eye Color)
-  "pid", //(Passport ID)
-  // "cid" //(Country ID)
-]
+const invalidPassports = [
+  "eyr:1972 cid:100\nhcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+  "iyr:2019\nhcl:#602927 eyr:1967 hgt:170cm\necl:grn pid:012533040 byr:1946",
+  "hcl:dab227 iyr:2012\necl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+  "hgt:59cm ecl:zzz\neyr:2038 hcl:74454a iyr:2023\npid:3556412378 byr:2007\n"
+];
 
-validPassports = 0;
+const validPassports = [
+  "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f",
+  "eyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+  "hcl:#888785\nhgt:164cm byr:2001 iyr:2015 cid:88\npid:545766238 ecl:hzl\neyr:2022",
+  "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719\n"
+];
+
+const validations = {
+  byr: function(birthYear) { 
+      return birthYear >= 1920 && birthYear <=  2002;
+  }, 
+  iyr: function(issueYear) { 
+    return issueYear >= 2010 && issueYear <=  2020;
+  }, 
+  eyr: function(expirationYear) {
+    return expirationYear >=  2020 && expirationYear <=  2030;
+  }, 
+  hgt: function(heightWithUnits) {
+    if(!heightWithUnits) { return false }
+
+    const [_, height, units] = heightWithUnits.split(/(\d+)/);
+    switch(units) {
+      case 'cm':
+        return height >= 150 && height <=  193;
+      case 'in':
+        return height >= 59 && height <=  76;
+      default:
+        return false;
+    }
+  }, 
+  hcl: function(hairColor){
+    return hairColor && !!hairColor.match(/^#[0-9a-f]{6}$/);
+  },
+  ecl: function(eyeColor){
+    return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(eyeColor);
+  }, 
+  pid: function (passportId){
+    return passportId && !!passportId.match(/^\d{9}$/);
+  }, 
+}
+
+totalValidPassports = 0;
 
 for (const rawPassport of passportList) {
   const passport = createPassportObject(rawPassport);
-  const passportKeys = Object.keys(passport);
-  
-  const validPassport = requiredFields.every(key => passportKeys.includes(key));
-  
-  if (validPassport) { validPassports++ };
+  console.log('###############')
+  const validPassport = Object.entries(validations).every(([field, validator]) => {
+    console.log(field, passport[field],  validator(passport[field]));
+    return validator(passport[field]);
+  });
+  console.log('###############')
+  if (validPassport) { totalValidPassports++ };
 }
 
 function createPassportObject(rawPassport) {
@@ -326,4 +365,4 @@ function createPassportObject(rawPassport) {
   }, {})
 }
 
-console.log("🚀 ~ file: day4.js ~ line 309 ~ validPassports", validPassports)
+console.log("🚀 ~ file: day4.js ~ line 309 ~ validPassports", totalValidPassports)
